75e3c9276a3c4dede1f28ebd9e6c19b3b126771e
David Boureau <bdavidxyz@gmail.com>
2023-09-07 14:25:35 +0200

    works!

---
+++ components/CustomRulePage.jsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Link from 'next/link'
+import { RulePage } from 'publicodes-react'
+import engine from '../utils/engine'
+
+export default function CustomRulePage(props) {
+  return (
+    <RulePage
+      documentationPath="/documentation"
+      rulePath={"prix"}
+      engine={engine}
+      language="fr"
+      renderers={{
+        Head,
+        Link: ({ to, children }) => <Link href={to}>{children}</Link>
+      }}
+    />
+  )
+}
--- pages/documentation/[...slug].jsx
+++ pages/documentation/[...slug].jsx
@@ -1,24 +1,16 @@
-import Head from 'next/head';
-import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { RulePage } from 'publicodes-react'
-import engine from '../../utils/engine'
+import dynamic from 'next/dynamic'
 
 export default function Documentation() {
   const router = useRouter()
 
   console.log('router.query.slug', router.query.slug);
 
+  const CustomRulePage = dynamic(() => import('../../components/CustomRulePage'), {
+    ssr: false,
+  })
+
   return <>
-    <RulePage
-      documentationPath="/documentation"
-      rulePath={"prix"}
-      engine={engine}
-      language="fr"
-      renderers={{
-        Head,
-        Link: ({ to, children }) => <Link href={to}><a>{children}</a></Link>
-      }}
-    />
+    <CustomRulePage></CustomRulePage>
   </>
 }
